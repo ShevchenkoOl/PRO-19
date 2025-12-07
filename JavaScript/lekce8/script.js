587df138c6828acc0eede8513919f18ee12c73c1
ShevchenkoOl <uzlabini@gmail.com>
2025-12-07 13:41:51 +0100

    lekce8

---
+++ JavaScript/lekce8/script.js
@@ -0,0 +1,47 @@
+// document // корневой объект
+// window.document // то же самое
+
+// const container = document.getElementById("cont");
+// console.log(container);
+
+// const container = document.getElementsByClassName("container");
+// console.log(container);
+// const cont2 = container.getElementsByClassName("div2");
+
+// const container = document.getElementsByTagName("div");
+// console.log(container);
+
+// const container = document.querySelector("div");
+// console.log(container);
+
+// const items = document.querySelectorAll(".item");
+// console.log(items);
+
+// const list = document.querySelector("#list"); // array
+// console.log(list);
+
+// const items = document.querySelectorAll(".item");
+// console.log(items[0]);
+
+// const firstItem = list.querySelector("#list > li:first-child");
+// console.log(firstItem);
+
+// const logo = document.querySelector("#logo");
+// logo.src = "https://imgd.aeplcdn.com/600x337/n/cw/ec/131825/be-6-exterior-right-front-three-quarter-6.png?isig=0&q=80";
+// logo.alt = "this another picture";
+// logo.id = "id2";
+// logo.width = "200";
+// console.log(logo);
+
+
+// const div2 = document.querySelector(".div2");
+// div2.textContent = "Hello IT Step!"
+// console.log(div2.textContent);
+
+// div2.style.color = "green";
+// div2.style.fontSize = "36px";
+// console.log(div2.style);
+
+// div2.classList.add("div3"); // добавляет класс
+// div2.classList.remove("div2"); // удаляет класс
+// div2.classList.toggle("div1"); // переключатель - если есть заберёт, если нет - добавит
