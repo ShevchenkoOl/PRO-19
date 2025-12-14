6a3372d3b08b5d4b49ef0ad86f93079d86dbf28b
ShevchenkoOl <uzlabini@gmail.com>
2025-12-14 15:00:35 +0100

    added lekce 10

---
+++ JavaScript/lekce10/script.js
@@ -0,0 +1,49 @@
+// const btn_toggle = document.querySelector("#btn-toggle");
+// const btn_clear = document.querySelector("#btn-clear");
+
+
+// const savedTheme = localStorage.getItem("theme");
+
+// if(savedTheme === "dark"){
+//     document.body.classList.add("dark")
+// }
+
+
+// btn_toggle.addEventListener("click", ()=>{
+//     document.body.classList.toggle("dark");
+
+//     const theme = document.body.classList.contains("dark") ? "dark" : "light"
+//     localStorage.setItem("theme", theme);
+//     localStorage.setItem("lan", "cz")
+
+// });
+
+// btn_clear.addEventListener("click", ()=>{
+//     localStorage.clear();
+//     // localStorage.removeItem("theme");
+// });
+
+// console.log(localStorage.length);
+
+
+// const user =[
+//     { name: "Alex", age: 25},
+//     { name: "Alex", age: 25},
+// ];
+
+
+// console.log(user); // objc
+// console.log(JSON.stringify(user)); // '{"name":"Alex","age":25}'
+// const str = JSON.stringify(user);
+// console.log(JSON.parse(str));   // objc
+
+const img = [
+    {id: 1, src:"img1.jpg", alt:"text", likes: 1541, downloads:45211452},
+    {id: 1, src:"img1.jpg", alt:"text", likes: 1541, downloads:45211452},
+    {id: 1, src:"img1.jpg", alt:"text", likes: 1541, downloads:45211452},
+    {id: 1, src:"img1.jpg", alt:"text", likes: 1541, downloads:45211452},
+    {id: 1, src:"img1.jpg", alt:"text", likes: 1541, downloads:45211452},
+    {id: 1, src:"img1.jpg", alt:"text", likes: 1541, downloads:45211452},
+    {id: 1, src:"img1.jpg", alt:"text", likes: 1541, downloads:45211452},
+    {id: 1, src:"img1.jpg", alt:"text", likes: 1541, downloads:45211452},
+]
--- JavaScript/lekce9/script.js
+++ JavaScript/lekce9/script.js
@@ -120,26 +120,129 @@
 // Список: определить, по какому LI кликнули Есть список UL с 5 элементами.
 // При клике выводи: event.target.textContent
 
-const list = document.querySelector("#list");
+// const list = document.querySelector("#list");
 
-list.firstChild is 
-// list.addEventListener("click", (e)=>{
-//     console.log(e.target);
+// list.addEventListener("click", (e) => {
+//   if (e.target.tagName !== "LI") return;
 
-// //     if(e.target.tagName === "LI"){
-// //         console.log(e.target.textContent);
-// //     }
-    
-// //    for (const li of list.children){
-// //     li.classList.remove("active")
-// //    }
-// // if(e.target.className !== "active"){
-// //     e.target.classList.add("active");
-// // } else {
-// //      e.target.classList.remove("active");
-// // }
+//   const clickedLi = e.target;
+
+//   // если кликнули по УЖЕ активному элементу — toggle
+//   if (clickedLi.classList.contains("active")) {
+//     clickedLi.classList.remove("active");
+//     return;
+//   }
+
+//   // если кликнули по другому — снимаем active со всех
+//   for (const li of list.children) {
+//     li.classList.remove("active");
+//   }
+
+//   // и добавляем активный только текущему
+//   clickedLi.classList.add("active");
 // });
 
 
 
+//  Events forms
+// const input = document.getElementById('input');
 
+// // input.addEventListener("input", (event)=> console.log(event.target.value));
+// input.addEventListener("change", (event)=> console.log(event.target.value));
+
+
+
+// Практика
+// const input = document.querySelector("#myInput");
+// const output = document.querySelector("#output");
+
+// input.addEventListener("change", (e)=>{
+//   console.log(e.target.value);
+//   console.log(input.value);
+//   // output.textContent = input.value;
+//   input.value='';
+// });
+
+// input.addEventListener("input", ()=>{
+//   output.textContent = input.value;
+// });
+
+// Submit
+// const form = document.querySelector("#form");
+// const input = document.querySelector("#input");
+// const btn = document.querySelector("#btn");
+
+// form.addEventListener("submit", (e)=>{
+//   e.preventDefault();
+//   console.log("Отправлено", input.value)
+//   input.value="";
+// });
+
+// const checkbox = document.querySelector("#inp");
+// checkbox.addEventListener("change", ()=>{
+// console.log(checkbox.checked);
+// })
+
+// placeholder — серый текст-подсказка.
+// checked — для чекбоксов.
+// files — для <input type="file">.
+// checkbox.placeholder = "Enter the text"
+// console.log(checkbox.placeholder)
+
+// задача Чтение checkbox Есть форма: имя email checkbox "Согласен с правилами" При submit: Если чекбокс не отмечен — выводим ошибку.
+
+// const form = document.querySelector('#signupForm');
+// const div = document.querySelector('#error');
+// const checkbox = document.querySelector('[name="agree"]');
+
+// form.addEventListener("submit", (event)=>{
+// event.preventDefault();
+
+// if(checkbox.checked){
+//   div.textContent = ""
+  
+// } else {
+//   div.textContent = "ERROR"
+//   return
+// }
+
+// console.log("Send");
+// form.reset();
+// })
+
+
+
+
+// options
+
+// const btn = document.querySelector("#btn");
+
+// btn.addEventListener("click", ()=>{
+//   console.log("pressed");
+// }, {once:true})
+
+// const parent = document.querySelector("#parent");
+
+// parent.addEventListener("click", ()=> console.log("parent"))
+// parent.addEventListener("click", ()=> console.log("parent-capture"), {capture: true})
+// btn.addEventListener("click", ()=> console.log("btn-child"))
+
+// form.addEventListener("submit", ()=>{}, {passive:false})
+
+https://developer.mozilla.org/ru/docs/Web/API/EventTarget/addEventListener#%D0%BF%D0%B0%D1%80%D0%B0%D0%BC%D0%B5%D1%82%D1%80_options
+
+
+
+
+
+
+// 2. Форма логина
+// Есть два поля: email и password.
+// При submit:
+//  - отменить поведение по умолчанию
+//  - вывести в консоль объект вида:
+// { email: "...", password: "..." }
+
+// 3. Выпадающий список (select) Есть select: USA Germany Ukraine При submit вывести выбранную страну.
+
+// 5. Создание карточки после отправки формы Форма содержит 3 поля: title description imageURL После submit создавай DOM-карточку: <div class="card"> <h3>...</h3> <p>...</p> <img src="..."> </div>
